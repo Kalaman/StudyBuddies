7d9c20cf3c5a50e62afd9ccb8f5c2f1536a99c6c
GroovyChuk <alasdair333@hotmail.com>
2018-04-04 19:50:15 +0200

    added onClickListener for add Button + possiblity to hide button

--- app/src/main/java/com/th_koeln/studybuddies/LearngroupRecyclerAdapter.java
+++ app/src/main/java/com/th_koeln/studybuddies/LearngroupRecyclerAdapter.java
@@ -1,11 +1,17 @@
 package com.th_koeln.studybuddies;
 
+import android.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -16,22 +22,28 @@ import java.util.ArrayList;
 public class LearngroupRecyclerAdapter extends RecyclerView.Adapter<LearngroupRecyclerAdapter.LearngroupViewHolder>{
 
     ArrayList<Learngroup> arrayListLearngroup;
+    boolean showButton;
 
-    public LearngroupRecyclerAdapter(ArrayList <Learngroup> arrayList) {
+    public LearngroupRecyclerAdapter(ArrayList <Learngroup> arrayList, boolean showButton) {
         arrayListLearngroup = arrayList;
+        this.showButton = showButton;
     }
 
     @Override
     public LearngroupViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_learngroup, parent, false);
-
         return new LearngroupViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(LearngroupViewHolder holder, int position) {
         Learngroup currentLearngroup = arrayListLearngroup.get(position);
+        if (showButton)
+            holder.getAddButton().setVisibility(View.VISIBLE);
+        else
+            holder.getAddButton().setVisibility(View.GONE);
 
+        holder.lid = currentLearngroup.getLid();
         holder.textViewTitle.setText(currentLearngroup.getTitle());
         holder.textViewDescription.setText(currentLearngroup.getDescription());
         holder.textViewCourse.setText(currentLearngroup.getCourse());
@@ -57,18 +69,45 @@ public class LearngroupRecyclerAdapter extends RecyclerView.Adapter<LearngroupRe
         TextView textViewStudentCount;
         TextView textViewCourse;
         TextView textViewDatetime;
+        Button addLearngroup;
+        String lid;
         View view;
 
-        public LearngroupViewHolder(View itemView) {
+        public LearngroupViewHolder(final View itemView) {
             super(itemView);
+            addLearngroup = (Button) itemView.findViewById(R.id.add_learngroup);
             textViewTitle = (TextView)itemView.findViewById(R.id.textViewLearngroupTitle);
             textViewDescription = (TextView)itemView.findViewById(R.id.textViewLearngroupDescription);
             textViewLocation = (TextView)itemView.findViewById(R.id.textViewLocation);
             textViewStudentCount = (TextView)itemView.findViewById(R.id.textViewStudentCount);
             textViewCourse = (TextView)itemView.findViewById(R.id.textViewCourse);
             textViewDatetime = (TextView)itemView.findViewById(R.id.textViewDatetime);
-
             view = itemView;
+
+            addLearngroup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DatabaseActions databaseActions = new DatabaseActions();
+                    databaseActions.joinGroup(itemView.getContext(), new DatabaseActions.DBRequestListener() {
+                        @Override
+                        public void onDBRequestFinished(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String message = jsonObject.getString("message");
+                                int success = jsonObject.getInt("success");
+
+                                Toast.makeText(itemView.getContext(),message,Toast.LENGTH_SHORT).show();
+
+                            } catch (Exception e) {};
+                        }
+                    },MainActivity.studentName, lid, true);
+                }
+            });
         }
+
+        public Button getAddButton(){
+            return addLearngroup;
+        }
+
     }
 }
